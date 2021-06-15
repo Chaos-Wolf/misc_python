f1cfa0728f1ad658e7fd9d0428e184f922777085
Chaos-Wolf <hamishclark9@gmail.com>
2021-06-15 12:18:33 -0400

    start of mass emailer GUI start of email tools

---
+++ email_tools.py
@@ -0,0 +1,5 @@
+
+
+def email_login(user_name):
+	print("from",user_name)
+	return True
--- excel_tools.py
+++ excel_tools.py
@@ -1,7 +1,16 @@
 import pandas 
+import tkinter
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 def ArrayFromExcel(filepath,column):
 	data = pandas.read_excel(filepath)
 	array = []
 	for i in range(data.shape[0]):
 		array.append(data.at[i,column])
 	return array
+
+def Get_Filepath_Gui():
+    """Open a file for editing."""
+    filepath = askopenfilename(filetypes=[("Excel Files", "*.xlsx"), ("All Files", "*.*")])
+    if not filepath:
+        return
+    return(filepath)
