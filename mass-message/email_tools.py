6c6bf74127b7828c1c6a73fe586e141fe842c058
Chaos-Wolf <hamishclark9@gmail.com>
2021-06-25 14:33:16 -0400

    functioning properly now, is able to send mass emails either from direct imput or by excel document gmail security is still a problem but i am working on that

--- mass-message/email_tools.py
+++ mass-message/email_tools.py
@@ -13,9 +13,23 @@ def email_login(username):
 		return False
 	window.destroy()
 def email_login_gui(user_name):
+	
 	return True
-	email_tools.email_send(From_Email.get(), Subject_Email.get(), Email_Txt.get("1.0","end-1c"), BCC_Email.get())
-def email_send(From,Subject,BCC):
-	yag = yagmail.SMTP(From)
-	yag.send(subject=Subject,contents="this is a test",bcc=BCC)
+def email_send(To,From,Subject,Contents,CC,BCC):
+	if To == "":
+		To = From
+	try:
+		yag = yagmail.SMTP(From)
+		yag.send(to=To,subject=Subject,contents=Contents,cc=CC,bcc=BCC)
+		sent_window = tk.Tk()
+		sent_window.title("Sent")
+		sent_window.geometry('50x50')
+		sent_label = tk.Label(sent_window,text="Emails Sent")
+		sent_label.grid(row=0,column=0)
+	except Exception as e:
+		print("error")
+		error_window = tk.Tk()
+		error_window.title("Error")
+		error_label = tk.Label(error_window, text=e)
+		error_label.grid(row=0,column=0)
 	
--- mass-message/mass_messagener.py
+++ mass-message/mass_messagener.py
@@ -2,26 +2,35 @@ import tkinter as tk
 from tkinter import ttk
 import excel_tools
 import email_tools
-email_list = []
+
+def email_list_create(List):
+	x = List.splitlines(False)
+	return x
+		
+
 def importer(column):
 	filepath = excel_tools.Get_Filepath_Gui()
 	if not filepath:
 		return
 	pull = excel_tools.ArrayFromExcel(filepath,column)
+	return (pull)
+def bcc_importer(c):
+	pull = importer(c)
 	for i in pull:
 		BCC_Email.insert(tk.END, i)
 		if i != pull[-1]:
-			BCC_Email.insert(tk.END, ", ")
+			BCC_Email.insert(tk.END, "\n")
 		print(i)
-	global email_list 
-	email_list = pull
-	return (pull)
+	return True
 
-def bcc_importer():
-	return importer('email')
-
-def cc_importer():
-	return importer('email')
+def cc_importer(c):
+	pull = importer(c)
+	for i in pull:
+		CC_Email.insert(tk.END, i)
+		if i != pull[-1]:
+			CC_Email.insert(tk.END, "\n")
+		print(i)
+	return True
 #main window
 window = tk.Tk()
 window.title("Mass Messenger")	
@@ -42,6 +51,9 @@ email.rowconfigure((0,5),weight=1)
 #email frame, from, list of to and login
 fr_email_ft = tk.Frame(email,relief=tk.RAISED,bd=2)
 fr_email_ft.grid(row=0,column=0)
+
+fr_email_body = tk.Frame(email,relief=tk.RAISED,bd=2)
+fr_email_body.grid(row=1,column=0)
 #from
 tk.Label(fr_email_ft, text="From").grid(row=0,column=0)
 From_Email = tk.Entry(fr_email_ft)
@@ -52,24 +64,24 @@ tk.Label(fr_email_ft, text="To").grid(row=0,column=3)
 To_Email = tk.Entry(fr_email_ft)
 To_Email.grid(row=0,column=4)
 #cc
-tk.Label(fr_email_ft, text="CC").grid(row=0,column=5)
-CC_Email = tk.Entry(fr_email_ft)
-CC_Email.grid(row=0,column=6)
+CC_Email = tk.Text(fr_email_body, width='25')
+CC_Email.grid(row=1,column=1)
 #bcc
-tk.Label(fr_email_ft, text="BCC").grid(row=1,column=0)
-BCC_Email = tk.Entry(fr_email_ft)
-BCC_Email.grid(row=1,column=1)
+BCC_Email = tk.Text(fr_email_body, width='25')
+BCC_Email.grid(row=1,column=2)
 
-tk.Label(fr_email_ft, text="Subject").grid(row=1,column=3)
+tk.Label(fr_email_ft, text="Subject").grid(row=0,column=5)
 Subject_Email = tk.Entry(fr_email_ft)
-Subject_Email.grid(row=1,column=4)
+Subject_Email.grid(row=0,column=6)
 #mass loader from a excel doc
-Import = tk.Button(fr_email_ft, text="Import",command=lambda: importer('Email')).grid(row=1,column=2)
-email_txt_edit_label = tk.Label(email, text="Message body").grid(column=0, row=2,)
+BCC_Import = tk.Button(fr_email_body, text="BCC Import",command=lambda: bcc_importer('Email')).grid(row=0,column=2)
+CC_Import = tk.Button(fr_email_body, text="CC Import",command=lambda: cc_importer('Email')).grid(row=0,column=1)
+email_txt_edit_label = tk.Label(fr_email_body, text="Message body").grid(column=0, row=0,)
 #message body
-Body_Email= tk.Text(email).grid(row=3,column=0)
+Body_Email= tk.Text(fr_email_body)
+Body_Email.grid(row=1,column=0)
 
-Send_Button = tk.Button(fr_email_ft, text="Send", command=lambda: email_tools.email_send(From_Email.get(), Subject_Email.get(), email_list)).grid(row=1,column=6)
+Send_Button = tk.Button(fr_email_ft, text="Send", command=lambda: email_tools.email_send(To_Email.get(),From_Email.get(), Subject_Email.get(), Body_Email.get("1.0","end-1c"),email_list_create(CC_Email.get("1.0","end-1c")),email_list_create(BCC_Email.get("1.0","end-1c")))).grid(row=0,column=7)
 
 
 
