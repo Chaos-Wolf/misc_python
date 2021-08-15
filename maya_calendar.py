536ae17b3fb782785f877a11c062d55b05f4468c
Chaos-Wolf <hamishclark9@gmail.com>
2021-08-15 16:34:57 -0400

    change to objects

--- maya_calendar.py
+++ maya_calendar.py
@@ -1,14 +1,31 @@
-from datetime import date
-today = date.today()
-bce = date.fromisoformat('2012-12-21')
-total_days = 1872000+abs(today-bce).days
-print(total_days)
-baktun = total_days//144000
-baktun_re = total_days%144000
-katun = baktun_re//7200
-katun_re = baktun_re%7200
-tun = katun_re//360
-tun_re = katun_re%360
-uinal = tun_re//20
-kin = tun_re%20
-print(baktun,".",katun,".",tun,".",uinal,".",kin)
+import datetime
+class Maya_Date(object):
+	def __init__(self,baktun,katun,tun,uinal,kin):
+		self.baktun = baktun
+		self.katun = katun
+		self.tun = tun
+		self.uinal = uinal
+		self.kin = kin
+	def __init__(self,total_days):
+		self.baktun = total_days//144000
+		self.katun = (total_days%144000)//7200
+		self.tun = ((total_days%144000)%7200)//360
+		self.uinal = (((total_days%144000)%7200)%360)//20
+		self.kin = (((total_days%144000)%7200)%360)%20
+	def __str__(self):
+		return(str(self.baktun)+"."+str(self.katun)+"."+str(self.tun)+"."+str(self.uinal)+"."+str(self.kin))
+	
+def today_maya():
+	today = datetime.date.today()
+	bce = datetime.date.fromisoformat('2012-12-21')
+	total_days = 1872000+abs(today-bce).days
+	final_date = Maya_Date(total_days)
+	return(final_date)
+	
+def mayan_conversion(year,month,day):
+	today = datetime.date(year,month,day)
+	end = datetime.date.fromisoformat('2012-12-21')
+	total_days = 1872000+(today-end).days
+	final_date = Maya_Date(total_days)
+	return(final_date)
+
